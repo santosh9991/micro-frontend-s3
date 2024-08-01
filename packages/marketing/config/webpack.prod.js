b4ee52c98b3f7de43701083784b37e7650432889
santosh kesireddy <santoshkesireddy@santoshs-MBP.lan>
2024-07-31 19:51:57 -0700

    marketing deployment added

--- packages/marketing/config/webpack.prod.js
+++ packages/marketing/config/webpack.prod.js
@@ -5,6 +5,10 @@ const commonConfig = require('./webpack.common');
 const packageJson = require('../package.json')
 const prodConfig = {
     mode: 'production',
+    output:{
+        filename: '[name].[contenthash].js',
+        publicPath: '/marketing/latest/'
+    },
     plugins: [
         new ModuleFederationPlugin({
             name: 'marketing',
