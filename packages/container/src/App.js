8c363ba8892d67d3bc8e97ea9a14bcab2df3feea
santosh kesireddy <santoshkesireddy@santoshs-MBP.lan>
2024-07-31 18:47:06 -0700

    container.yml updated

---
+++ packages/container/src/App.js
@@ -0,0 +1,9 @@
+import React from "react";
+import MarketingApp from "./containers/MarketingApp"
+export default ()=>{
+    return <div>
+        <h1>Hey there!!! container</h1>
+        <hr></hr>
+        <MarketingApp/>
+    </div>
+}
