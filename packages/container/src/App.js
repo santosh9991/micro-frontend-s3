9b36ba4cf41313fa3c4d2aab1a2b63034ec49a56
santosh kesireddy <santoshkesireddy@santoshs-MBP.lan>
2024-07-31 20:01:39 -0700

    production domain env var added to container.yml

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import MarketingApp from "./containers/MarketingApp"
 export default ()=>{
     return <div>
-        <h1>Hey there!!!</h1>
+        <h1>Hey there!!!!</h1>
         <hr></hr>
         <MarketingApp/>
     </div>
