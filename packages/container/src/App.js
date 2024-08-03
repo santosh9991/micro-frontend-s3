56057d29c8fcfb2d41e7094915c795cdc662952b
santosh kesireddy <santoshkesireddy@santoshs-MacBook-Pro.local>
2024-08-03 06:56:56 -0700

    css name collisiion fiix addressed

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -2,11 +2,19 @@ import React from "react";
 import { BrowserRouter } from "react-router-dom";
 import MarketingApp from "./containers/MarketingApp";
 import Header from "./containers/Header"
+import { StylesProvider, createGenerateClassName } from "@material-ui/core";
+
+const generateClassName = createGenerateClassName({
+    productionPrefix: 'co'
+})
 export default ()=>{
-    return <BrowserRouter>
+    return <BrowserRouter generateClassName={generateClassName}>
+    <StylesProvider>
     <div>
         <Header/>
         <MarketingApp/>
     </div>
+    </StylesProvider>
+    
     </BrowserRouter>
 }
